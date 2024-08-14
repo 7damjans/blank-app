5be047ae4e26907bae31dd0be90c3eec3131bb5f
7damjans <ddpas@schools.vic.edu.au>
2024-08-14 10:29:43 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,6 @@ css="""
 </style>
 """
 
-st.markdown(page_bg_img, unsafe_allow_html=True)
-
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
