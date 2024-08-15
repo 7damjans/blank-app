f0446f06942d995a6652417307c2cee9b352ca24
7damjans <ddpas@schools.vic.edu.au>
2024-08-15 11:32:29 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(
     page_title="Light Project",
     page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSdbM0Bqr7Q7mCAouhY1p_x_poXPrxinl9a7Q&s",
+    layout="wide",
 )
 st.title("🎈 My new app")
 st.write(
