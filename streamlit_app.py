03585c57f99cd72fe6844150cbeb8d2a2cc5d4b2
7damjans <ddpas@schools.vic.edu.au>
2024-08-15 11:26:56 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(
     page_title="Hello",
-    page_icon="🤣",
+    page_icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFdhiY218vl2AyWxlTZBWa2ZqPbVxD0rWwIg&s",
 )
 st.title("🎈 My new app")
 st.write(
