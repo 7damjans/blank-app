3a21d4fd74d1c440201b673e911abb98e4e42a51
7damjans <ddpas@schools.vic.edu.au>
2024-08-15 11:50:55 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,28 +17,7 @@ st.set_page_config(
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    df1 = pd.DataFrame(
-    np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
-    )
-
-    my_table = st.table(df1)
-
-    df2 = pd.DataFrame(
-        np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
-    )
-
-    my_table.add_rows(df2)
-    my_chart = st.vega_lite_chart(
-    {
-        "mark": "line",
-        "encoding": {"x": "a", "y": "b"},
-        "datasets": {
-            "some_fancy_name": df1,  # <-- named dataset
-        },
-        "data": {"name": "some_fancy_name"},
-    }
-)
-my_chart.add_rows(some_fancy_name=df2)  # <-- name used as keyword
+   st.header("Aqsqsddsn owl")
 
 with col2:
     st.header("A dog")
