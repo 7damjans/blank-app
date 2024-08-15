f8921d40bee5c5e200082a69d3bfcbad3b134e29
7damjans <ddpas@schools.vic.edu.au>
2024-08-15 11:49:09 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,17 @@ st.set_page_config(
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
+    df1 = pd.DataFrame(
+    np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
+    )
+
+    my_table = st.table(df1)
+
+    df2 = pd.DataFrame(
+        np.random.randn(50, 20), columns=("col %d" % i for i in range(20))
+    )
+
+    my_table.add_rows(df2)
 
 with col2:
     st.header("A dog")
