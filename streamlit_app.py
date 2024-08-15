3bcf065f17fdf214ae24bf7ecba86d0dded1447d
7damjans <ddpas@schools.vic.edu.au>
2024-08-15 11:43:32 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.DataFrame(
-   np.random.randn(11, 6), columns=("col %d" % i for i in range(5))
+   np.random.randn(11, 6), columns=("col %d" % i for i in range(6))
 )
 
 st.table(df)
